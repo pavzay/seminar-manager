d8dffca47274ba6ce9a9cda59fff7d4a181cbad0
Pavel Zaichyk <zaychick.pav@yandex.ru>
2018-06-07 17:39:55 +0300

    correct gateway service and add frontend service

---
+++ frontend-service/src/main/java/com/github/pavzay/seminarmanager/FrontendApplication.java
@@ -0,0 +1,15 @@
+package com.github.pavzay.seminarmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+}
