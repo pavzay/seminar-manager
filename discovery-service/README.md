47cfa1ce500a3043d673e74daa0c6a17d158c926
Pavel Zaichyk <zaychick.pav@yandex.ru>
2018-08-23 17:12:33 +0300

    move frontend to gateway service remove frontend service change spring version

---
+++ discovery-service/README.md
@@ -0,0 +1,3 @@
+# Discovery Service
+
+Spring Eureka dashboard: http://localhost:8761
--- gateway-service/src/main/java/com/github/pavzay/seminarmanager/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/github/pavzay/seminarmanager/gateway/GatewayApplication.java
@@ -3,8 +3,10 @@ package com.github.pavzay.seminarmanager.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
